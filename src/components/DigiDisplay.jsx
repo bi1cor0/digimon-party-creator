e36b56ddb78697983cd0e88651780db24ccff34d
yeahbudd01 <billcorcoro@comcast.net>
2024-07-22 02:42:36 -0700

    Edited some text to display on to the application.

--- src/components/DigiDisplay.jsx
+++ src/components/DigiDisplay.jsx
@@ -1,9 +1,19 @@
 export default function DigiDisplay({ digimon }) {
      const loaded = () => {
+        //setting up ? variables to check if the necessary data exists before trying to access it. 
+        const level = digimon?.levels?.[0]?.level || 'Unknown';
+        const attribute = digimon?.attributes?.[0]?.attribute || 'Unknown';
+        const description = digimon?.descriptions?.[1]?.description || 'No description available';
+
         return (
         <>
-            <h1>{digimon.name}</h1>
+            <h2>{digimon.name}</h2>
             <img src={digimon.images[0].href} alt={digimon.name} />
+            <ul>
+                <li>Level: {level}</li>
+                <li>Attribute: {attribute}</li>
+                <li>Description: {description}</li>
+            </ul>
         </>
         );
         };
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -22,6 +22,14 @@ export default function Form(props) {
   };
 
     return (
+        <>
+        <h1>Welcome to the Digimon Search and Info Application!</h1>
+        <h3>Wondering what Digimon are? Here's a handy link: 
+        <a href="https://wikimon.net/Digimon_(species)r" target="_blank"> Wikimon: The Digimon Wiki</a>
+        </h3>
+        <h4>This application uses the Digimon API or DAPI and serves as an introduction into what the Digimon are.</h4>
+        <h4>Please go ahead and search for any Digimon you are curious about. Or if you don't know any Digimon, go ahead and enter in a number from 1 to 1460!</h4>
+
         <div>
             <form onSubmit={handleSubmit}>
                 <input 
@@ -32,5 +40,6 @@ export default function Form(props) {
                 <input type='submit' value='Search'/>
             </form>
         </div>
+        </>
   )
 }
