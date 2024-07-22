0912ca5d8ff923757472fc9f4e17f11f13789366
yeahbudd01 <billcorcoro@comcast.net>
2024-07-21 22:26:06 -0700

    Updated DigiDisplay to display the Digimon picture

--- src/components/DigiDisplay.jsx
+++ src/components/DigiDisplay.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+export default function DigiDisplay({ digimon }) {
+     const loaded = () => {
+        return (
+        <>
+            <h1>{digimon.name}</h1>
+            <img src={digimon.images[0].href} alt={digimon.name} />
+        </>
+        );
+        };
 
-function DigiDisplay() {
-  return (
-    <div>DigiDisplay</div>
-  )
-}
-
-export default DigiDisplay
+        const loading = () => {
+            return <h1>No Digimon to Show</h1>
+        }
+        return digimon ? loaded() : loading();
+  }
