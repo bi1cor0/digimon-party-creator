7ce7822ced79d2e722042c418caa4768d5ee93d0
yeahbudd01 <billcorcoro@comcast.net>
2024-07-21 21:09:18 -0700

    Function to get the Digimon data from the DAPI.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,18 @@ import Form from './components/Form'
 export default function App() {
   const [digimon, setDigimon] = useState(null)
 
+    // Function to get Digimon names
+    const getDigimon = async(searchTerm) => {
+      // Make fetch request and store the response
+      const response = await fetch(
+        `https://digi-api.com/api/v1/digimon/${searchTerm}`
+      );
+      // Parse JSON response into a JavaScript object
+      const data = await response.json();
+      // Set the Movie state to the received data
+      setDigimon(data);
+    };
+
   return (
       <>
         <div className='App'>
